a8e3605a8e0f0e44ace9e1df2c963e262cf2b23c
ryuken73 <ryuken73@gmail.com>
2023-02-18 19:53:03 +0900

    update: first working version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.cjs.jsx';
+import styled from 'styled-components';
+import { useSearchParams } from 'react-router-dom';
+
+const Container = styled.div`
+`
+const DEFAULT_URL = "https://scrollyvideo.js.org/goldengate.mp4"
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mp4Url = searchParams.get('url') || DEFAULT_URL;
+  const useWebCodecs = JSON.parse(searchParams.get('smooth')) || false;
+  const height = searchParams.get('length') || 800;
+  const transitionSpeed = searchParams.get('transition') || 10;
+  console.log(mp4Url, useWebCodecs, typeof(useWebCodecs), height, transitionSpeed)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ height: `${height}vh` }}>
+      <ScrollyVideo 
+        src={mp4Url} 
+        useWebCodecs={useWebCodecs} 
+        transitionSpeed={transitionSpeed}
+        frameThreshold={0.1}
+        debug={true}
+      />
+    </Container>
   );
 }
 
