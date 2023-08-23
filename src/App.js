9522530971c4a70eb7666c25c8724d42a21ec106
ryuken73 <ryuken73@gmail.com>
2023-08-24 00:09:52 +0900

    update: choose direction (h or v)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.cjs.jsx';
-import ScrollyVideo from './scrolly/src/ScrollyVideo.jsx';
+import ScrollyVideoV from 'scrolly-video/dist/ScrollyVideo.esm.jsx';
+import ScrollyVideoH from './scrolly/src/ScrollyVideo.jsx';
 import styled from 'styled-components';
 import { useSearchParams } from 'react-router-dom';
 
@@ -11,13 +11,20 @@ function App() {
   const [searchParams, setSearchParams] = useSearchParams();
   const mp4Url = searchParams.get('url') || DEFAULT_URL;
   const useWebCodecs = JSON.parse(searchParams.get('smooth')) || false;
-  const height = searchParams.get('length') || 800;
-  const transitionSpeed = searchParams.get('transition') || 10;
+  const direction = searchParams.get('direction') || 'h';
+  const height = direction === 'h' ? 300 : searchParams.get('length') || 800;
+  const width = direction === 'v' ? 100 : searchParams.get('length') || 300;
+  const transitionSpeed = searchParams.get('transition') || 8;
+  const ScrollyVideo = (props) => {
+    return direction === 'v' ? 
+      <ScrollyVideoV {...props} /> :
+      <ScrollyVideoH {...props} />
+  }
   console.log(mp4Url, useWebCodecs, typeof(useWebCodecs), height, transitionSpeed)
   return (
     // <Container style={{ height: `${height}vh`, width: '800vw' }}>
-    <Container style={{ height: `${height}vh`, width: '200vw' }}>
-      <ScrollyVideo 
+    <Container style={{ height: `${height}vh`, width: `${width}vw` }}> 
+      <ScrollyVideo
         src={mp4Url} 
         useWebCodecs={true} 
         transitionSpeed={transitionSpeed}
